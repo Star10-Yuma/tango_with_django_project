d3280a06ddbb3e221f27cf48ce8ab2af54506f48
Hussain ALFAYLY <2724542a@student.gla.ac.uk>
2023-01-26 14:09:52 +0000

    Chapter 6 models,views and templates structure completed

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from rango.models import Category, Page, PageAdmin
 
+#This customises the admin interface
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
 # Register your models here.
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
--- rango/models.py
+++ rango/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.contrib import admin
+from django.template.defaultfilters import slugify
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique = True)
 
+    #overrides the save method to get to slugify the names of the categories so that the URL can not have spaces in its category names
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 #Fixes name from categorys to categories in admin page
     class Meta:
         verbose_name_plural = "Categories"
